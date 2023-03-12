309f40887b6892eb24c38e35454570eca3ac3d49
Do-hyun-Kim <dohyun8032@gmail.com>
2023-03-13 00:54:21 +0900

    feat: Tuist init Project 생성

---
+++ Targets/HobbyloopIOSKit/Tests/HobbyloopIOSKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class HobbyloopIOSKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("HobbyloopIOSKit", "HobbyloopIOSKit")
+    }
+}
---
+++ Targets/HobbyloopIOSUI/Tests/HobbyloopIOSUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class HobbyloopIOSUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("HobbyloopIOSUI", "HobbyloopIOSUI")
+    }
+}
