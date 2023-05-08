a94789215e2f2613e61e11b3b7e74ed70df0bbd4
Do-hyun-Kim <dohyun8032@gmail.com>
2023-05-08 19:37:30 +0900

    feat: 📝LoginViewController File 추가 or Presentation, Application 영역 Group화 - BaseViewController HPCommonUI 모듈화

---
+++ App/Sources/Presentation/Login/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  Hobbyloop
+//
+//  Created by Kim dohyun on 2023/05/08.
+//
+
+import UIKit
+
+import HPCommonUI
+import ReactorKit
+
+
+final class LoginViewController: BaseViewController<LoginViewReactor> {
+    
+    //MARK: Property
+    
+    
+    //MARK: LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: Configure
+    private func configure(){
+        
+        
+    }
+    
+    public override func bind(reactor: LoginViewReactor) {
+        
+    }
+}
+
+
+
---
+++ App/Sources/Presentation/Login/Reactor/LoginViewReactor.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewReactor.swift
+//  Hobbyloop
+//
+//  Created by Kim dohyun on 2023/05/08.
+//
+
+import Foundation
+
+import ReactorKit
+import RxCocoa
+
+
+final class LoginViewReactor: Reactor {
+   
+    //MARK: Action
+    typealias Action = NoAction
+    
+    //MARK: State
+    struct State {}
+    
+    //MARK: InitialState
+    var initialState: State = .init()
+    
+    
+    
+}
