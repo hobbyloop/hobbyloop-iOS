927ce01b381939988e2e36e87422a9ac49f5ed38
Do-hyun-Kim <dohyun8032@gmail.com>
2023-05-25 16:33:36 +0900

    feat: 🎨 LoginViewController UI Configure ✍️ LoginViewController AutoLayout Code 추가 및 Assets Color or Image 추가

--- App/Sources/Presentation/Login/LoginViewController.swift
+++ App/Sources/Presentation/Login/LoginViewController.swift
@@ -9,10 +9,46 @@ import UIKit
 
 import HPCommonUI
 import Then
+import SnapKit
 import ReactorKit
 
 final class LoginViewController: BaseViewController<LoginViewReactor> {
-    //MARK: Property
+    // MARK: Property
+    private lazy var loginStckView: UIStackView = UIStackView().then {
+        $0.distribution = .equalSpacing
+        $0.axis = .vertical
+        $0.spacing = 15
+    }
+    private let logoImageView: UIImageView = UIImageView().then {
+        $0.image = HPCommonUIAsset.logo.image.withRenderingMode(.alwaysOriginal)
+        $0.contentMode = .scaleToFill
+    }
+    private let backgroundImageView: UIImageView = UIImageView().then {
+        $0.image = HPCommonUIAsset.background.image.withRenderingMode(.alwaysOriginal)
+        $0.contentMode = .scaleToFill
+    }
+    private let kakaoLoginButton: UIButton = UIButton(type: .custom).then {
+        $0.setImage(HPCommonUIAsset.kakao.image.withRenderingMode(.alwaysOriginal), for: .normal)
+    }
+    
+    private let googleLoginButton: UIButton = UIButton(type: .custom).then {
+        $0.setImage(HPCommonUIAsset.google.image.withRenderingMode(.alwaysOriginal), for: .normal)
+    }
+    
+    private let naverLoginButton: UIButton = UIButton(type: .custom).then {
+        $0.setImage(HPCommonUIAsset.naver.image.withRenderingMode(.alwaysOriginal), for: .normal)
+        
+    }
+    
+    private let appleLoginButton: UIButton = UIButton(type: .custom).then {
+        $0.setImage(HPCommonUIAsset.apple.image.withRenderingMode(.alwaysOriginal), for: .normal)
+    }
+    
+    private let underLineView: UIView = UIView().then {
+        $0.backgroundColor = HPCommonUIAsset.lightSeparator.color
+    }
+    
+    
     
     override init(reactor: LoginViewReactor?) {
         defer { self.reactor = reactor }
@@ -35,7 +71,40 @@ final class LoginViewController: BaseViewController<LoginViewReactor> {
     }
     
     // MARK: Configure
-    private func configure() { }
+    private func configure() {
+        
+        [backgroundImageView, logoImageView ,loginStckView, underLineView].forEach {
+            view.addSubview($0)
+        }
+        
+        [kakaoLoginButton, googleLoginButton, naverLoginButton, appleLoginButton].forEach {
+            loginStckView.addArrangedSubview($0)
+        }
+        backgroundImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        logoImageView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.topMargin).offset(80)
+            $0.centerX.equalToSuperview()
+            $0.width.height.equalTo(50)
+        }
+        
+        loginStckView.snp.makeConstraints {
+            $0.left.equalToSuperview().offset(45)
+            $0.right.equalToSuperview().offset(-45)
+            $0.bottom.equalTo(underLineView.snp.top).offset(-25)
+            $0.height.equalTo(245)
+        }
+        
+        underLineView.snp.makeConstraints {
+            $0.left.equalToSuperview().offset(20)
+            $0.right.equalToSuperview().offset(-20)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottomMargin).offset(-42)
+            $0.height.equalTo(1)
+        }
+        
+    }
     
     public override func bind(reactor: LoginViewReactor) { }
 }
