dbb2137861c7c7286334ef13887a408ebcf47c35
namgeon1106 <namgeon1106@naver.com>
2023-06-06 20:41:03 +0900

    asset: image 추가 및 HPCommonUIAsset에 static 프로퍼티 추가

---
+++ UI/HPCommonUI/Sources/Extensions/HPCommonUIAsset+.swift
@@ -0,0 +1,13 @@
+//
+//  HPCommonUIAsset+.swift
+//  HPCommonUI
+//
+//  Created by 김남건 on 2023/06/06.
+//
+
+import UIKit
+
+extension HPCommonUIAsset {
+    static let selectedPage = HPCommonUIImages(name: "selectedPage")
+    static let unselectedPage = HPCommonUIImages(name: "unselectedPage")
+}
