409c6a032711c02ab44da09938e4ee7f6ed974d6
Do-hyun-Kim <dohyun8032@gmail.com>
2023-06-19 21:20:02 +0900

    feat: 🎨 SignUpBottomSheetView Configure

---
+++ Core/HPCommon/Sources/SignUp/SignUpBottomSheetView.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpBottomSheetView.swift
+//  HPCommon
+//
+//  Created by Kim dohyun on 2023/06/19.
+//
+
+import UIKit
+
+import Then
+import SnapKit
+
+public final class SignUpBottomSheetView: UIViewController {
+    
+    
+    //MARK: Property
+    
+    
+    
+    //MARK: LifeCycle
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    //MARK: Configure
+    private func configure() {}
+    
+    
+}
