1d3d2e56d8fb980223f9e7d0bc091f3f19061a0a
Do-hyun-Kim <dohyun8032@gmail.com>
2023-07-28 17:59:56 +0900

    feat: 🎨 HPCalendarView UI Configure or Image Assets 추가

--- UI/HPCommonUI/Sources/Common/Calendar/HPCalendarView.swift
+++ UI/HPCommonUI/Sources/Common/Calendar/HPCalendarView.swift
@@ -8,11 +8,30 @@
 import UIKit
 
 import Then
+import SnapKit
 
-final class HPCalendarView: UIView {
+public final class HPCalendarView: UIView {
     
     
     // MARK: Property
+    private var calendarMonthLabel: UILabel = UILabel().then {
+        $0.font = HPCommonUIFontFamily.Pretendard.bold.font(size: 16)
+        $0.textAlignment = .center
+        $0.numberOfLines = 1
+    }
+    
+    private var previousButton: UIButton = UIButton(type: .custom).then {
+        $0.setTitle("", for: .normal)
+        $0.setImage(HPCommonUIAsset.previousArrow.image, for: .normal)
+    }
+    
+    private var nextButton: UIButton = UIButton(type: .custom).then {
+        $0.setTitle("", for: .normal)
+        $0.setImage(HPCommonUIAsset.nextArrow.image, for: .normal)
+
+    }
+    
+    
     private var calendarCollectionViewLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout().then {
         $0.itemSize = CGSize(width: 35, height: 35)
         $0.minimumLineSpacing = 0
@@ -25,14 +44,18 @@ final class HPCalendarView: UIView {
         $0.showsVerticalScrollIndicator = false
     }
     
-    override init(frame: CGRect) {
+    public override init(frame: CGRect) {
         super.init(frame: frame)
+        configure()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    
+    //MARK: Configure
+    private func configure() { }
     
 }
 
