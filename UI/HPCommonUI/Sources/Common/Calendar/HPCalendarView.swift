fd45e206590b2ba3984cbcd236ef3eb8ced172c0
Do-hyun-Kim <dohyun8032@gmail.com>
2023-07-28 16:54:35 +0900

    feat: 📆 HPCalendarView CollectionView UI Configure

--- UI/HPCommonUI/Sources/Common/Calendar/HPCalendarView.swift
+++ UI/HPCommonUI/Sources/Common/Calendar/HPCalendarView.swift
@@ -12,7 +12,18 @@ import Then
 final class HPCalendarView: UIView {
     
     
-    private lazy var calendarCollectionView: UICollectionView = UICollectionView(frame: self.frame)
+    // MARK: Property
+    private var calendarCollectionViewLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout().then {
+        $0.itemSize = CGSize(width: 35, height: 35)
+        $0.minimumLineSpacing = 0
+        $0.minimumInteritemSpacing = 0
+    }
+    
+    private lazy var calendarCollectionView: UICollectionView = UICollectionView(frame: self.frame, collectionViewLayout: calendarCollectionViewLayout).then {
+        $0.isScrollEnabled = false
+        $0.showsHorizontalScrollIndicator = false
+        $0.showsVerticalScrollIndicator = false
+    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
