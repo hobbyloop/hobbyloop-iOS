cf22d69f9ee12ecd8d78441d2470ea9b218969ee
namgeon1106 <namgeon1106@naver.com>
2023-09-03 16:13:14 +0900

    feat: 내 정보 수정 화면에 custom navigation bar 추가

--- App/Sources/Presentation/MyPage/UserInfoEditViewController.swift
+++ App/Sources/Presentation/MyPage/UserInfoEditViewController.swift
@@ -9,7 +9,36 @@ import UIKit
 import HPCommon
 import HPCommonUI
 
-class UserInfoEditViewController: UIViewController {    
+class UserInfoEditViewController: UIViewController {
+    // MARK: - custom navigation bar
+    private let backButton = UIButton(configuration: .plain()).then {
+        $0.configuration?.image = HPCommonUIAsset.leftarrow.image
+        
+        $0.snp.makeConstraints {
+            $0.width.equalTo(21)
+            $0.height.equalTo(22)
+        }
+    }
+    
+    private let navigationTitleLabel = UILabel().then {
+        $0.text = "내 정보 수정"
+        $0.font = HPCommonUIFontFamily.Pretendard.bold.font(size: 16)
+    }
+    
+    private lazy var customNavigationBar = UIView().then {
+        [backButton, navigationTitleLabel].forEach($0.addSubview(_:))
+        
+        navigationTitleLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        
+        backButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(24)
+        }
+    }
+    
+    // MARK: - input view
     private let nameInputView = SignUpInfoView(titleType: .name, filled: true).then {
         $0.titleLabel.text = "이름"
         $0.textFiledView.text = "김지원"
@@ -30,9 +59,17 @@ class UserInfoEditViewController: UIViewController {
         $0.textFiledView.text = "010-1234-5678"
     }
     
+    // MARK: - life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        
+        view.addSubview(customNavigationBar)
+        customNavigationBar.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.top.equalToSuperview().offset(44)
+            $0.height.equalTo(56)
+        }
         
         let inputStack = UIStackView()
         inputStack.axis = .vertical
