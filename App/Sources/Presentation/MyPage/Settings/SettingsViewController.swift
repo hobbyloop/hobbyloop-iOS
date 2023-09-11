f74fc4dfa65e72a029037d70624ced93a31dc99b
namgeon1106 <namgeon1106@naver.com>
2023-09-11 20:28:27 +0900

    feat: 설정 화면 custom navigation bar 추가

--- App/Sources/Presentation/MyPage/Settings/SettingsViewController.swift
+++ App/Sources/Presentation/MyPage/Settings/SettingsViewController.swift
@@ -6,9 +6,46 @@
 //
 
 import UIKit
+import Then
+import HPCommonUI
 
 class SettingsViewController: UIViewController {
+    private let backButton = UIButton(configuration: .plain()).then {
+        $0.configuration?.image = HPCommonUIAsset.leftarrow.image
+        
+        $0.snp.makeConstraints {
+            $0.width.equalTo(21)
+            $0.height.equalTo(22)
+        }
+    }
+    
+    private let navigationTitleLabel = UILabel().then {
+        $0.text = "설정"
+        $0.font = HPCommonUIFontFamily.Pretendard.bold.font(size: 16)
+    }
+    
+    private lazy var customNavigationBar = UIView().then {
+        [backButton, navigationTitleLabel].forEach($0.addSubview(_:))
+        
+        navigationTitleLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        
+        backButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(24)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        view.addSubview(customNavigationBar)
+        
+        customNavigationBar.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.top.equalToSuperview().offset(44)
+            $0.height.equalTo(56)
+        }
     }
 }
