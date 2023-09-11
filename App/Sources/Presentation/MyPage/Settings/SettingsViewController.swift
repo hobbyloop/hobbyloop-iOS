4cc161ddb25b00dbddea9a611da27f466be8e446
namgeon1106 <namgeon1106@naver.com>
2023-09-11 21:42:01 +0900

    feat: 앱 설정 섹션 UI 구현

--- App/Sources/Presentation/MyPage/Settings/SettingsViewController.swift
+++ App/Sources/Presentation/MyPage/Settings/SettingsViewController.swift
@@ -37,6 +37,27 @@ class SettingsViewController: UIViewController {
         }
     }
     
+    // MARK: - 앱 설정 섹션 UI
+    private lazy var appSectionHeader = sectionHeader(title: "앱 설정", bottomMargin: 11)
+    
+    private let appAlarmSwitch = HPSwitch().then {
+        $0.transform = CGAffineTransform(scaleX: 35 / 51, y: 21 / 31)
+    }
+    private lazy var appAlarmMenu = menuWithSwitch(name: "앱 알림 설정", switchView: appAlarmSwitch)
+    
+    private let adAlarmSwitch = HPSwitch().then {
+        $0.transform = CGAffineTransform(scaleX: 35 / 51, y: 21 / 31)
+    }
+    private lazy var adAlarmMenu = menuWithSwitch(name: "광고 알림 설정", switchView: adAlarmSwitch)
+    private lazy var homeViewMenu = menuWithArrow(name: "홈 화면 설정")
+    private lazy var versionMenu = menuWithArrow(name: "버전정보")
+    
+    private let bottomMarginView = UIView().then {
+        $0.snp.makeConstraints {
+            $0.height.equalTo(12)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
@@ -47,5 +68,99 @@ class SettingsViewController: UIViewController {
             $0.top.equalToSuperview().offset(44)
             $0.height.equalTo(56)
         }
+        
+        let backgroundView = UIView()
+        backgroundView.backgroundColor = HPCommonUIAsset.lightBackground.color
+        
+        view.addSubview(backgroundView)
+        backgroundView.snp.makeConstraints {
+            $0.top.equalTo(customNavigationBar.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        let appSectionStack = UIStackView()
+        appSectionStack.backgroundColor = .systemBackground
+        appSectionStack.axis = .vertical
+        appSectionStack.alignment = .fill
+        appSectionStack.spacing = 0
+        
+        [appSectionHeader, appAlarmMenu, adAlarmMenu, homeViewMenu, versionMenu, bottomMarginView].forEach(appSectionStack.addArrangedSubview(_:))
+        backgroundView.addSubview(appSectionStack)
+        
+        appSectionStack.snp.makeConstraints {
+            $0.top.equalTo(customNavigationBar.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+        }
+    }
+    
+    private func sectionHeader(title: String, bottomMargin: CGFloat) -> UIView {
+        let view = UIView()
+        let titleLabel = UILabel()
+        titleLabel.text = title
+        titleLabel.font = HPCommonUIFontFamily.Pretendard.bold.font(size: 18)
+        
+        view.addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(21)
+            $0.leading.equalToSuperview().offset(27)
+            $0.bottom.equalToSuperview().offset(-bottomMargin)
+        }
+        
+        return view
+    }
+    
+    private func menuWithSwitch(name: String, switchView: HPSwitch) -> UIView {
+        let view = UIView()
+        view.snp.makeConstraints {
+            $0.height.equalTo(60)
+        }
+        
+        let nameLabel = UILabel()
+        nameLabel.text = name
+        nameLabel.font = HPCommonUIFontFamily.Pretendard.medium.font(size: 16)
+        
+        [nameLabel, switchView].forEach(view.addSubview(_:))
+        
+        nameLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(26)
+        }
+        
+        switchView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().offset(-23)
+        }
+        
+        return view
+    }
+    
+    private func menuWithArrow(name: String) -> UIButton {
+        let view = UIButton()
+        view.snp.makeConstraints {
+            $0.height.equalTo(60)
+        }
+        
+        let nameLabel = UILabel()
+        nameLabel.text = name
+        nameLabel.font = HPCommonUIFontFamily.Pretendard.medium.font(size: 16)
+        
+        let arrowView = UIImageView(image: HPCommonUIAsset.rightarrow.image.withTintColor(HPCommonUIAsset.buttonTitle.color))
+        
+        [nameLabel, arrowView].forEach(view.addSubview(_:))
+        
+        nameLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(26)
+        }
+        
+        arrowView.snp.makeConstraints {
+            $0.width.equalTo(7.23)
+            $0.height.equalTo(12.63)
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().offset(-23.28)
+        }
+        
+        return view
     }
 }
