ae514798a843f7a941a3583ec79622030f3d5447
namgeon1106 <namgeon1106@naver.com>
2023-09-11 19:41:59 +0900

    feat: SettingsViewController 추가

---
+++ App/Sources/Presentation/MyPage/Settings/SettingsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsViewController.swift
+//  Hobbyloop
+//
+//  Created by 김남건 on 2023/09/11.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
