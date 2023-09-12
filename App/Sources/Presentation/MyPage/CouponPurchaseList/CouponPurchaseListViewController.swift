57385a941b9114290a7b695a535e5f369b3d3e7b
namgeon1106 <namgeon1106@naver.com>
2023-09-12 18:19:30 +0900

    feat: CouponPurchaseListViewController 추가

---
+++ App/Sources/Presentation/MyPage/CouponPurchaseList/CouponPurchaseListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CouponPurchaseListViewController.swift
+//  Hobbyloop
+//
+//  Created by 김남건 on 2023/09/12.
+//
+
+import UIKit
+
+class CouponPurchaseListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
