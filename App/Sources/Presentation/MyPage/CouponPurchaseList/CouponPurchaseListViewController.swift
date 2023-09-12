01a6a8c5a54129d514a83758918e060b1f89c681
namgeon1106 <namgeon1106@naver.com>
2023-09-12 18:39:41 +0900

    feat: 커스텀 navigation bar 추가

--- App/Sources/Presentation/MyPage/CouponPurchaseList/CouponPurchaseListViewController.swift
+++ App/Sources/Presentation/MyPage/CouponPurchaseList/CouponPurchaseListViewController.swift
@@ -6,9 +6,51 @@
 //
 
 import UIKit
+import HPCommonUI
 
 class CouponPurchaseListViewController: UIViewController {
+    // MARK: - custom navigation bar
+    private let backButton = UIButton(configuration: .plain()).then {
+        $0.configuration?.image = HPCommonUIAsset.leftarrow.image
+        
+        $0.snp.makeConstraints {
+            $0.width.equalTo(21)
+            $0.height.equalTo(22)
+        }
+    }
+    
+    private let navigationTitleLabel = UILabel().then {
+        $0.text = "이용권 구매내역"
+        $0.font = HPCommonUIFontFamily.Pretendard.bold.font(size: 16)
+    }
+    
+    private lazy var customNavigationBar = UIView().then {
+        [backButton, navigationTitleLabel].forEach($0.addSubview(_:))
+        
+        navigationTitleLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        
+        backButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(24)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        layoutNavigationBar()
+    }
+    
+    // MARK: - layout
+    private func layoutNavigationBar() {
+        view.addSubview(customNavigationBar)
+        
+        customNavigationBar.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.top.equalToSuperview().offset(44)
+            $0.height.equalTo(56)
+        }
     }
 }
