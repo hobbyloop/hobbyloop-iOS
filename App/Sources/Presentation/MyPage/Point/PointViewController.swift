d77d72b0b93a3b60250c704c4c21a2828894278e
namgeon1106 <namgeon1106@naver.com>
2023-09-22 19:15:24 +0900

    feat: lightBackground 색 적용된 background view 추가

--- App/Sources/Presentation/MyPage/Point/PointViewController.swift
+++ App/Sources/Presentation/MyPage/Point/PointViewController.swift
@@ -9,20 +9,33 @@ import UIKit
 import HPCommonUI
 
 final class PointViewController: UIViewController {
+    // MARK: - navigation bar back button
     private let backButton = UIButton(frame: CGRect(x: 0, y: 0, width: 33, height: 22)).then {
         $0.setImage(HPCommonUIAsset.leftarrow.image, for: .normal)
+    }
+    
+    // MARK: - background view
+    private let backgroundView = UIView().then {
+        $0.backgroundColor = HPCommonUIAsset.lightBackground.color
     }
     
     // MARK: - life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .systemBackground
+        view.addSubview(backgroundView)
+        
+        backgroundView.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+        }
         configNavigationBar()
         addActions()
     }
     
     // MARK: - layout
     private func configNavigationBar() {
+        navigationController?.navigationBar.backgroundColor = .systemBackground
         navigationItem.title = "포인트"
         navigationItem.setHidesBackButton(true, animated: false)
         navigationItem.leftBarButtonItem = UIBarButtonItem(customView: backButton)
