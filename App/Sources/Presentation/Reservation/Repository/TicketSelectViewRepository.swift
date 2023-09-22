f89cc16b648361bc023c04f91dbb53111fade417
Do-hyun-Kim <dohyun8032@gmail.com>
2023-09-22 19:10:44 +0900

    feat: TicketSelectRouter API 통신 Router or TicketSelectViewRepository Configure

--- App/Sources/Presentation/Reservation/Repository/TicketSelectViewRepository.swift
+++ App/Sources/Presentation/Reservation/Repository/TicketSelectViewRepository.swift
@@ -7,14 +7,21 @@
 
 import Foundation
 
+import HPNetwork
+import RxSwift
+
 
 public protocol TicketSelectViewRepo: AnyObject {
+    var disposeBag: DisposeBag { get }
     
-    
+    var networkService: APIService { get }
 }
 
 
 public final class TicketSelectViewRepository: TicketSelectViewRepo {
-    
+    //MARK: Property
+    public var disposeBag: DisposeBag = DisposeBag()
+    public var networkService: APIService = APIClient.shared
+
     
 }
---
+++ Core/HPNetwork/Sources/Routers/TicketSelectRouter.swift
@@ -0,0 +1,67 @@
+//
+//  TicketSelectRouter.swift
+//  HPNetwork
+//
+//  Created by Kim dohyun on 2023/09/22.
+//
+
+import Foundation
+
+import Alamofire
+import HPCommon
+
+
+public enum TicketSelectRouter {
+    case getFacilityList
+    case getInstructorList(_ id: Int)
+}
+
+
+
+extension TicketSelectRouter: Router {
+    
+    
+    public var method: HTTPMethod {
+        //TODO: 현재 모든 API가 모두 GET이기에 Default get
+        return .get
+    }
+    
+    public var path: String {
+        switch self {
+        case .getFacilityList:
+            return "/api/v1/facility/list/all"
+            
+        case .getInstructorList:
+            return "/api/v1/instructor"
+        }
+    }
+    
+    public var parameters: HPParameterType {
+        switch self {
+        case let .getInstructorList(id):
+            return .query(
+                [
+                "centerId": id
+                ]
+            )
+        default:
+            return .none
+        }
+    }
+    
+    public var headers: HTTPHeaders {
+        var token: String = ""
+        do {
+            token = try CryptoUtil.makeDecryption(UserDefaults.standard.string(forKey: .accessToken))
+        } catch {
+            print(error.localizedDescription)
+        }
+        return [
+            "Authorization": "\(token)",
+            "Accept": "*/*"
+         ]
+    }
+    
+    
+    
+}
