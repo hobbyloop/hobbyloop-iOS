9857671b913de6c12974b9684ec0cd02278e85a0
namgeon1106 <namgeon1106@naver.com>
2024-05-12 13:20:59 +0900

    feat: 마이페이지 navigation bar UI 구현

--- App/Sources/Presentation/MyPage/MyPageViewController.swift
+++ App/Sources/Presentation/MyPage/MyPageViewController.swift
@@ -11,9 +11,30 @@ import HPCommonUI
 import Then
 
 final class MyPageViewController: UIViewController {
+    private let settingsButton = UIButton().then {
+        $0.setImage(HPCommonUIAsset.settingOutlind.image, for: [])
+        $0.tintColor = HPCommonUIAsset.gray100.color
+    }
+    
+    private lazy var settingsButtonItem = UIBarButtonItem(customView: settingsButton)
+    
     // MARK: - life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        configureNavigationBar()
+    }
+    
+    private func configureNavigationBar() {
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.titleTextAttributes = [
+            .foregroundColor: HPCommonUIAsset.gray100.color,
+            .font: HPCommonUIFontFamily.Pretendard.semiBold.font(size: 16)
+        ]
+        
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.compactAppearance = navBarAppearance
+        navigationItem.title = "프로필 수정"
+        navigationItem.rightBarButtonItem = settingsButtonItem
     }
 }
