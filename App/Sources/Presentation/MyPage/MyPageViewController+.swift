38452f6bc9ce7ced0ffba7cbb95239fcb0cb880f
namgeon1106 <namgeon1106@naver.com>
2024-05-12 22:19:55 +0900

    refactor: 2개의 버튼 메서드를 extension으로 분리

--- App/Sources/Presentation/MyPage/MyPageViewController+.swift
+++ App/Sources/Presentation/MyPage/MyPageViewController+.swift
@@ -9,112 +9,52 @@ import UIKit
 import HPCommonUI
 
 extension MyPageViewController {
-    /// 이미지와 텍스트가 세로 방향으로 나열된 버튼 반환.
-    func verticalStackButton(imageView: UIImageView, label: UILabel, topMargin: CGFloat = 0) -> UIButton {
-        let button = UIButton(type: .custom)
-        
-        button.addSubview(imageView)
-        button.addSubview(label)
-        
-        button.snp.makeConstraints {
-            $0.width.greaterThanOrEqualTo(37)
-            $0.height.equalTo(51)
+    /// 포인트, 이용권, 리뷰 버튼에 해당
+    func KeyValueButton(title: String, subtitle: String) -> UIButton {
+        return UIButton(configuration: .plain()).then {
+            $0.configuration?.attributedTitle = AttributedString.init(title, attributes: .init([
+                .font: HPCommonUIFontFamily.Pretendard.medium.font(size: 14),
+                .foregroundColor: HPCommonUIAsset.gray80.color
+            ]))
+            
+            $0.configuration?.attributedSubtitle = AttributedString.init(subtitle, attributes: .init([
+                .font: HPCommonUIFontFamily.Pretendard.bold.font(size: 16),
+                .foregroundColor: HPCommonUIAsset.gray100.color
+            ]))
+            
+            $0.configuration?.titleAlignment = .center
+            $0.configuration?.titlePadding = 10
+            $0.configuration?.contentInsets = .zero
         }
-        
-        imageView.snp.makeConstraints {
-            $0.top.equalTo(button.snp.top).offset(topMargin)
-            $0.centerX.equalTo(button.snp.centerX)
-        }
-        
-        label.snp.makeConstraints {
-            $0.leading.equalTo(button.snp.leading)
-            $0.trailing.equalTo(button.snp.trailing)
-            $0.bottom.equalTo(button.snp.bottom)
-        }
-        
-        return button
     }
     
-    /// 이미지, 설명 텍스트, 개수 텍스트가 가로로 나열된 버튼 반환.
-    func horizontalStackButton(imageView: UIImageView, description: String, countLabel: UILabel) -> UIButton {
-        let button = UIButton(type: .custom)
-        button.contentMode = .center
-        
-        let descriptionLabel = UILabel()
-        descriptionLabel.text = description
-        descriptionLabel.font = HPCommonUIFontFamily.Pretendard.medium.font(size: 14)
-        descriptionLabel.textColor = HPCommonUIAsset.couponInfoLabel.color
-        
-        
-        let contentStack = UIStackView()
-        contentStack.axis = .horizontal
-        contentStack.alignment = .center
-        contentStack.spacing = 5
-        
-        [imageView, descriptionLabel, countLabel].forEach(contentStack.addArrangedSubview(_:))
-        
-        button.addSubview(contentStack)
-        
-        contentStack.snp.makeConstraints {
-            $0.top.equalTo(button.snp.top)
-            $0.leading.equalTo(button.snp.leading)
-            $0.trailing.equalTo(button.snp.trailing)
-            $0.bottom.equalTo(button.snp.bottom)
+    /// 보관함, 리뷰, 수업 내역 버튼에 해당
+    func ArrowedButton(title: String, textColor: UIColor = HPCommonUIAsset.gray100.color) -> UIButton {
+        let button = UIButton(configuration: .plain()).then {
+            $0.configuration?.attributedTitle = AttributedString.init(title, attributes: .init([
+                .font: HPCommonUIFontFamily.Pretendard.bold.font(size: 16),
+                .foregroundColor: textColor
+            ]))
+            
+            
+            $0.configuration?.imagePlacement = .trailing
+            $0.configuration?.contentInsets = .init(top: 21, leading: 16, bottom: 22, trailing: 26)
+            
+            $0.configuration?.imagePadding = view.bounds.width - ($0.titleLabel?.intrinsicContentSize.width ?? 0) - 50
+            $0.setImage(HPCommonUIAsset.rightarrow.image.withRenderingMode(.alwaysTemplate).imageWith(newSize: .init(width: 8, height: 14)), for: [])
+            $0.tintColor = textColor
         }
         
-        return button
-    }
-    
-    func reviewCountText(_ count: Int) -> NSAttributedString {
-        let newString = "리뷰 \(count)"
-        let reviewRange = (newString as NSString).range(of: "리뷰 ")
-        let countRange = (newString as NSString).range(of: "\(count)")
+        let bottomEdgeView = UIView()
+        bottomEdgeView.backgroundColor = HPCommonUIAsset.gray20.color
+        button.addSubview(bottomEdgeView)
         
-        let attributedString = NSMutableAttributedString(string: newString)
-        attributedString.addAttribute(.font, value: HPCommonUIFontFamily.Pretendard.bold.font(size: 14), range: reviewRange)
-        attributedString.addAttribute(.font, value: HPCommonUIFontFamily.Pretendard.bold.font(size: 14), range: countRange)
-        attributedString.addAttribute(.foregroundColor, value: HPCommonUIAsset.deepOrange.color, range: countRange)
-        
-        return attributedString
-    }
-    
-    func partHeaderButton(text: String) -> UIButton {
-        let button = UIButton()
-        
-        let label = UILabel()
-        label.text = text
-        label.font = HPCommonUIFontFamily.Pretendard.bold.font(size: 18)
-        
-        let arrowImageView = UIImageView(image: HPCommonUIAsset.rightarrow.image)
-        arrowImageView.snp.makeConstraints {
-            $0.width.equalTo(11)
-            $0.height.equalTo(17)
-        }
-        
-        [label, arrowImageView].forEach(button.addSubview(_:))
-        
-        label.snp.makeConstraints {
-            $0.top.equalTo(button.snp.top)
-            $0.bottom.equalTo(button.snp.bottom)
-            $0.leading.equalTo(button.snp.leading).offset(29)
-        }
-        
-        arrowImageView.snp.makeConstraints {
-            $0.centerY.equalTo(button.snp.centerY)
-            $0.trailing.equalTo(button.snp.trailing).offset(-27)
-        }
-        
-        return button
-    }
-    
-    func stackViewDivider() -> UIView {
-        let view = UIView()
-        view.backgroundColor = HPCommonUIAsset.stackDivider.color
-        
-        view.snp.makeConstraints {
+        bottomEdgeView.snp.makeConstraints {
+            $0.bottom.equalToSuperview()
             $0.height.equalTo(1)
+            $0.leading.trailing.equalToSuperview()
         }
         
-        return view
+        return button
     }
 }
