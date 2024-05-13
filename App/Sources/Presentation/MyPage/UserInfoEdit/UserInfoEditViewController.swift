25f41c619cd12fed45c4c217f5418d51310f5c4e
namgeon1106 <namgeon1106@naver.com>
2024-05-13 14:12:00 +0900

    feat: 내 정보 수정 화면 UI 구성

--- App/Sources/Presentation/MyPage/UserInfoEdit/UserInfoEditViewController.swift
+++ App/Sources/Presentation/MyPage/UserInfoEdit/UserInfoEditViewController.swift
@@ -39,12 +39,66 @@ final class UserInfoEditViewController: UIViewController {
         }
     }
     
+    private let nameView: SignUpInfoView = SignUpInfoView(titleType: .name)
+    private let nickNameView: SignUpInfoView = SignUpInfoView(titleType: .nickname)
+    private let birthDayView: SignUpInfoView = SignUpInfoView(titleType: .birthDay)
+    // TODO: picker view를 SignUpInfoView 안에 포함시키기
+    private let birthDayPickerView = UIDatePicker().then {
+        $0.datePickerMode = .date
+        $0.preferredDatePickerStyle = .wheels
+        $0.backgroundColor = .clear
+        $0.locale = .init(identifier: "ko-KR")
+        $0.timeZone = .autoupdatingCurrent
+        $0.backgroundColor = HPCommonUIAsset.gray20.color
+        $0.layer.cornerRadius = 8
+        $0.clipsToBounds = true
+        $0.isHidden = true
+    }
+    
+    private let phoneView: SignUpInfoView = SignUpInfoView(titleType: .phone)
+    private let certificateButton = HPNewButton(title: "인증번호 발송", style: .bordered)
+    private let phoneHStack = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 8
+        $0.alignment = .bottom
+        $0.distribution = .fill
+    }
+    
+    private let authCodeView: SignUpInfoView = SignUpInfoView(titleType: .authcode)
+    private let authCodeButton = HPNewButton(title: "인증번호 확인", style: .bordered).then {
+        $0.isEnabled = false
+    }
+    
+    private let authHStack = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 8
+        $0.alignment = .fill
+        $0.distribution = .fill
+    }
+    
+    private let phoneAuthVStack = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 12
+        $0.alignment = .fill
+    }
+    
+    private let inputFieldsVStack = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 24
+        $0.alignment = .fill
+    }
+    
+    private let editButton = HPNewButton(title: "수정완료", style: .primary).then {
+        $0.isEnabled = false
+    }
+    
     // MARK: - life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         
         configureNavigationBar()
+        layout()
     }
     
     private func configureNavigationBar() {
@@ -58,5 +112,73 @@ final class UserInfoEditViewController: UIViewController {
         navigationController?.navigationBar.compactAppearance = navBarAppearance
         navigationItem.title = "마이페이지"
         navigationItem.leftBarButtonItem = backButtonItem
+    }
+    
+    private func layout() {
+        [phoneView, certificateButton].forEach(phoneHStack.addArrangedSubview(_:))
+        
+        authCodeButton.snp.makeConstraints {
+            $0.width.equalTo(120)
+        }
+        
+        [authCodeView, authCodeButton].forEach(authHStack.addArrangedSubview(_:))
+        [phoneHStack, authHStack].forEach(phoneAuthVStack.addArrangedSubview(_:))
+        
+        [
+            nameView,
+            nickNameView,
+            birthDayView,
+            phoneView,
+        ].forEach {
+            $0.snp.makeConstraints {
+                $0.height.equalTo(76)
+            }
+        }
+        
+        [
+            nameView,
+            nickNameView,
+            birthDayView,
+            phoneAuthVStack
+        ].forEach(inputFieldsVStack.addArrangedSubview(_:))
+        
+        [
+            profileImageView,
+            photoEditButton,
+            inputFieldsVStack,
+            birthDayPickerView,
+            editButton
+        ].forEach(view.addSubview(_:))
+        
+        profileImageView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(5)
+            $0.centerX.equalToSuperview()
+        }
+        
+        photoEditButton.snp.makeConstraints {
+            $0.bottom.equalTo(profileImageView.snp.bottom).offset(-7)
+            $0.trailing.equalTo(profileImageView.snp.trailing).offset(7)
+        }
+        
+        certificateButton.snp.makeConstraints {
+            $0.width.equalTo(120)
+            $0.height.equalTo(48)
+        }
+        
+        inputFieldsVStack.snp.makeConstraints {
+            $0.top.equalTo(profileImageView.snp.bottom).offset(28)
+            $0.leading.trailing.equalToSuperview().inset(16)
+        }
+        
+        birthDayPickerView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.top.equalTo(birthDayView.snp.bottom).offset(4)
+        }
+        
+        editButton.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.bottom.equalToSuperview().offset(-32)
+            $0.height.equalTo(48)
+        }
     }
 }
