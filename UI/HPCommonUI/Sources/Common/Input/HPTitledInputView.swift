feddb8e13feabe5906f4977038ffaad374634daf
namgeon1106 <namgeon1106@naver.com>
2024-06-15 12:54:17 +0900

    feat: input view 내부의 date picker 제거

--- UI/HPCommonUI/Sources/Common/Input/HPTitledInputView.swift
+++ UI/HPCommonUI/Sources/Common/Input/HPTitledInputView.swift
@@ -43,17 +43,6 @@ public final class HPTitledInputView: UIView {
         $0.isHidden = true
     }
     
-    private let datePickerView = UIDatePicker().then {
-        $0.datePickerMode = .date
-        $0.preferredDatePickerStyle = .wheels
-        $0.locale = .init(identifier: "ko-KR")
-        $0.backgroundColor = HPCommonUIAsset.gray40.color
-        $0.isUserInteractionEnabled = true
-        $0.isHidden = true
-        $0.layer.cornerRadius = 10
-        $0.clipsToBounds = true
-    }
-    
     public var errorMessage: String? {
         get { errorMessageLabel.text }
         set {
@@ -79,19 +68,6 @@ public final class HPTitledInputView: UIView {
         
         textfield.placeholderText = placeholder
         layout()
-        
-        showDatePickerButton.addTarget(self, action: #selector(showOrHideDatePicker), for: .primaryActionTriggered)
-        
-        datePickerView.rx.date
-            .skip(1)
-            .map {
-                let dateFormatter = DateFormatter()
-                dateFormatter.dateFormat = "yyyy.MM.dd"
-                
-                return dateFormatter.string(from: $0)
-            }
-            .bind(to: textfield.rx.text)
-            .disposed(by: bag)
     }
     
     required init?(coder: NSCoder) {
@@ -122,16 +98,6 @@ public final class HPTitledInputView: UIView {
             $0.trailing.equalTo(textfield.snp.trailing).offset(-12)
             $0.centerY.equalTo(textfield.snp.centerY)
         }
-        
-        self.addSubview(datePickerView)
-        datePickerView.snp.makeConstraints {
-            $0.top.equalTo(textfield.snp.bottom).offset(4)
-            $0.leading.trailing.equalToSuperview()
-        }
-    }
-    
-    @objc private func showOrHideDatePicker() {
-        datePickerView.isHidden.toggle()
     }
     
     private func setAttributedTitle(_ title: String, isRequired: Bool) {
