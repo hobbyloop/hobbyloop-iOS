79b2264b47b83a857e8d7a0f627d5f3188dfa611
namgeon1106 <namgeon1106@naver.com>
2024-07-20 11:19:28 +0900

    feat: 서버로부터 이미지 받아오기

--- App/Sources/Presentation/MyPage/UserInfoEdit/Repository/UserInfoEditViewRepository.swift
+++ App/Sources/Presentation/MyPage/UserInfoEdit/Repository/UserInfoEditViewRepository.swift
@@ -13,15 +13,18 @@ import UIKit
 
 public protocol UserInfoEditViewRepo {
     var networkService: AccountClientService { get }
+    var imageService: ImageClientService { get }
     func getUserInfo() -> Observable<UserInfoEditViewReactor.Mutation>
     func issueVerificationID(phoneNumber: String) -> Observable<UserInfoEditViewReactor.Mutation>
     func verifyPhoneNumber(authCode: String, verificationID: String) -> Observable<UserInfoEditViewReactor.Mutation>
     func updateUserInfo(name: String, nickname: String, birthday: String, phoneNumber: String, profileImage: UIImage?) -> Observable<UserInfoEditViewReactor.Mutation>
+    func fetchImage(url: URL) -> Observable<UserInfoEditViewReactor.Mutation>
 }
 
 public final class UserInfoEditViewRepository: UserInfoEditViewRepo {
     
     public var networkService: AccountClientService = AccountClient.shared
+    public var imageService: ImageClientService = ImageClient.shared
     
     public func getUserInfo() -> Observable<UserInfoEditViewReactor.Mutation> {
         return self.networkService.getUserInfo()
@@ -30,16 +33,25 @@ public final class UserInfoEditViewRepository: UserInfoEditViewRepo {
                 // TODO: 에러 핸들링
                 return .empty()
             }
-            .flatMap { userInfo -> Observable<UserInfoEditViewReactor.Mutation> in
+            .flatMap { [weak self] userInfo -> Observable<UserInfoEditViewReactor.Mutation> in
+                guard let self else { return .empty() }
                 let birthdayArr = userInfo.birthday.components(separatedBy: "-")
+                var imageObservable: Observable<UserInfoEditViewReactor.Mutation> = .empty()
                 
-                return .of(
-                    .setName(userInfo.name),
-                    .setNickname(userInfo.nickname),
-                    .setPhoneNumber(userInfo.phoneNumber.replacingOccurrences(of: "-", with: "")),
-                    .setBirthday(birthdayArr[0] + "년 " + birthdayArr[1] + "월 " + birthdayArr[2] + "일"),
-                    .setProfileImageUrl(userInfo.profileImageUrl),
-                    .setIsValidPhoneNumber(true)
+                if let profileImageUrl = userInfo.profileImageUrl {
+                    imageObservable = self.fetchImage(url: profileImageUrl)
+                }
+                
+                return .merge(
+                    .of(
+                        .setName(userInfo.name),
+                        .setNickname(userInfo.nickname),
+                        .setPhoneNumber(userInfo.phoneNumber.replacingOccurrences(of: "-", with: "")),
+                        .setBirthday(birthdayArr[0] + "년 " + birthdayArr[1] + "월 " + birthdayArr[2] + "일"),
+                        .setProfileImageUrl(userInfo.profileImageUrl),
+                        .setIsValidPhoneNumber(true)
+                    ),
+                    imageObservable
                 )
             }
     }
@@ -92,5 +104,17 @@ public final class UserInfoEditViewRepository: UserInfoEditViewRepo {
         .map {
             return .userInfoSubmitted
         }
+    }
+    
+    public func fetchImage(url: URL) -> Observable<UserInfoEditViewReactor.Mutation> {
+        return self.imageService.fetchHPImage(url: url)
+            .asObservable()
+            .catch { _ in
+                // TODO: 에러 핸들링
+                return .empty()
+            }
+            .map { image in
+                return .setProfileImage(image)
+            }
     }
 }
